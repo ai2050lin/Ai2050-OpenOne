8fb8e8aa43524fc40765f54fe3e53bdb68e00acc
ai2050lin <ai2050lin@gmail.com>
2026-05-16 23:10:03 +0800

    20260516-5

---
+++ tests/glm5_temp/read_log.ps1
@@ -0,0 +1,10 @@
+$fs = [System.IO.FileStream]::new('d:\Ai2050\TransformerLens-Project\tests\glm5_temp\phase198_qwen3_log.txt', [System.IO.FileMode]::Open, [System.IO.FileAccess]::Read, [System.IO.FileShare]::ReadWrite)
+$sr = [System.IO.StreamReader]::new($fs)
+$content = $sr.ReadToEnd()
+$sr.Close()
+$fs.Close()
+$lines = $content -split "`n"
+$total = $lines.Count
+Write-Output "Total lines: $total"
+$start = [Math]::Max(0, $total - 30)
+for($i=$start; $i -lt $total; $i++){Write-Output $lines[$i]}
